f9382d2465941d2ff08273f95c3f6ef1de54f4d9
Aris-Arsen Kashari <arisyo13@gmail.com>
2026-02-16 14:09:14 +0100

    chore: eslint config

--- typings.d.ts
+++ typings.d.ts
@@ -9,6 +9,7 @@ declare module '@semantic-release/error' {
 }
 
 declare module 'plist' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const plist: any;
   export default plist;
 }
